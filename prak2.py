91282402845d07ca3dc3a79b18f05c4b474950f2
AdrianBustanil <rianbustanil450@gmail.com>
2025-10-07 15:08:10 +0700

    if

--- prak2.py
+++ prak2.py
@@ -1,3 +1,14 @@
 a = float(input("masukkan angka pertama: "))
 b = float(input("maukkan angka kedua: "))
 c = float(input("masukkan angka ketiga: "))
+
+if a > b and a > c:
+    largest = a
+elif b > a and b > c:
+    largest = b
+elif c > a and c > b:
+    largest = c 
+else:
+    largest = print("Tidak ada angka terbesar")
+
+print("Angka terbesar adalah:", largest)
