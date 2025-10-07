3584996d2d32a3d1a5d3fe4102d3573f3f78c888
AdrianBustanil <rianbustanil450@gmail.com>
2025-10-07 14:56:45 +0700

    print percentage

---
+++ p.py
@@ -0,0 +1,12 @@
+percentage = float(input("masukan persentasi nilai siswa: "))
+
+if percentage >= 90:
+    print("Excellent performance")
+elif percentage >= 80:
+    print("Very Good performance")
+elif percentage >= 70:
+    print("Goog performance")
+elif percentage >=60:
+    print("Average perfonmance")
+else:
+    print("Needs Inprovement")
